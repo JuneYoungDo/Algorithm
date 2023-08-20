01ad00a7bdb3c5fac49ebab8f5c2b261e5990566
JuneYoungDo <ehwnsdud1004@naver.com>
2023-08-20 22:38:54 +0900

    [2023.08.20] 10926_??!

---
+++ 10926_??!.cpp
@@ -0,0 +1,19 @@
+//
+// Created by June Young Do on 2023/08/20.
+//
+#include<iostream>
+
+using namespace std;
+#define endl '\n'
+
+string s;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> s;
+    cout << s + "??!" << endl;
+
+    return 0;
+}
